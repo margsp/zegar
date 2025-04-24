a71e2514057ba9bbe2ca63ecedb4bfb5a235ce8c
margsp <margsp@vivaldi.net>
2025-04-24 19:40:54 +0200

    Refactor js

--- script.js
+++ script.js
@@ -13,6 +13,10 @@ setInterval(function()
 
 function setClock()
 {
+  var time = getHours() + ":" + getMinutes();
+  document.getElementById("time").innerHTML = time;
+  
+  /*
   var date = new Date();
   var mins = date.getMinutes() + minOffset;
   if(date.getMinutes() == 59)
@@ -22,5 +26,19 @@ function setClock()
   }
   var minutesL = mins < 10 ? '0' : '';
   var time = (date.getHours() + hrOffset) + ":" + minutesL + mins;
-  document.getElementById("time").innerHTML = time;
+  */
+}
+
+function getHours()
+{
+	var hours = new Date().getHours();
+	return hours + hrOffset;
+}
+
+function getMinutes()
+{
+	var mins = new Date().getMinutes();
+	mins = mins + minOffset;
+	var minsL = mins < 10 ? '0' : '';
+	return minutesL + mins;
 }
