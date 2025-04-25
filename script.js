0e63a108a7524f82ee3c8e3279a2631c4db1f495
margsp <margsp@vivaldi.net>
2025-04-25 21:16:06 +0200

    Handle hours offset

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //**CONFIGURATION AREA**
 //set both values to 0 if your device's local time is OK
-var hrOffset = 0;
+var hrOffset = 2;
 var minOffset = 0;
 //END OF CONFIGURATION
 
@@ -15,24 +15,23 @@ function setClock()
 {
   var time = getHours() + ":" + getMinutes();
   document.getElementById("time").innerHTML = time;
-  
-  /*
-  var date = new Date();
-  var mins = date.getMinutes() + minOffset;
-  if(date.getMinutes() == 59)
-  {
-    hrOffset++;
-    mins = 0;
-  }
-  var minutesL = mins < 10 ? '0' : '';
-  var time = (date.getHours() + hrOffset) + ":" + minutesL + mins;
-  */
 }
 
 function getHours()
 {
 	var hours = new Date().getHours();
-	return hours + hrOffset;
+	if(hrOffset > 0)
+	{
+		if((hours + hrOffset) > 23) //make sure the clock is never like 24:30
+		{
+			hours =  0 + ((hours + hrOffset) - 24);
+		}
+		else
+		{
+			hours = hours + hrOffset;
+		}
+	}
+	return hours;
 }
 
 function getMinutes()
